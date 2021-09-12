3a8274047591e6425ab1fff3b651b012e318a7c9
Chen <hi@chenliang.ca>
2021-09-11 23:36:18 -0600

    trial

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '../styles/globals.css'
+import '../styles/footer.styles.css';
+import '../styles/home.styles.css';
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
